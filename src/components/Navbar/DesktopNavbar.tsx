85c58e6eea3aab3638b89c382c0d729880df28b2
Marian Noekel <mariannoekel@gmail.com>
2025-12-11 16:32:37 +0100

    add vercel.json

--- src/components/Navbar/DesktopNavbar.tsx
+++ src/components/Navbar/DesktopNavbar.tsx
@@ -1,6 +1,8 @@
 import Mail from "../../icons/Mail";
 import Phone from "../../icons/Phone";
 import { Link } from 'react-router-dom';
+https://jank-pi.vercel.app/Blog
+https://jank-pi.vercel.app/Blog
 
 export default function DesktopNavbar() {
   return (
