89c44ad2fb5e9a4e3b598afb86a61943867f3a23
Marian Noekel <mariannoekel@gmail.com>
2025-12-22 18:17:16 +0100

    add design changes to nav submenu

--- src/components/navbar/MobileNavbar.tsx
+++ src/components/navbar/MobileNavbar.tsx
@@ -11,7 +11,7 @@ import LinkGeoCheck from "../LinkGeoCheck";
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [selected, setSelected] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
 
   return (
     <div>
@@ -36,99 +36,115 @@ export default function Navbar() {
             </Link>
             <CircleX />
           </div>
-          <div className="p-8">
-            <ol className="flex flex-col gap-4 font-semibold text-lg">
+          <div className="">
+            <ol>
               <div
-                className="flex gap-2 items-center"
-                onClick={() => setSelected((prev) => !prev)}
+                className={`pt-4 rounded-t flex items-center justify-between ${
+                  isSelected
+                    ? "bg-slate-200 px-8"
+                    : "mx-8 border-b-2 border-slate-200"
+                }`}
+                onClick={() => setIsSelected((prev) => !prev)}
               >
-                <span>Leistungen</span>
+                <span className="font-semibold text-lg pb-2">Leistungen</span>
                 <FiChevronDown
                   className={`transition-transform size-6 ${
-                    selected ? "rotate-180" : ""
+                    isSelected ? "rotate-180" : ""
                   }`}
                 />
               </div>
-              <AnimatePresence>
-                {selected && (
-                  <motion.ol
-                    initial={{ height: 0, opacity: 0 }}
-                    animate={{ height: "auto", opacity: 1 }}
-                    exit={{ height: 0, opacity: 0 }}
-                    transition={{ duration: 0.3, ease: "easeInOut" }}
-                    className="ml-4 bg-white !font-medium"
-                  >
-                    <div className="flex flex-col gap-2">
-                      <h3 className="font-semibold">Google Platz 1</h3>
-                      <a href="#" className="mb-1 block text-neutral-400">
-                        SEO Berater für Google Rankings
-                      </a>
-                      <a href="#" className="block text-neutral-400">
-                        Google SEO Optimierung
-                      </a>
-                      <div
-                        className="text-neutral-400"
-                        onClick={() => setIsMenuOpen((prev) => !prev)}
-                      >
-                        <LinkSeoCheck />
+              <div className="flex flex-col gap-4 font-semibold text-lg">
+                <AnimatePresence>
+                  {isSelected && (
+                    <motion.ol
+                      initial={{ height: 0, opacity: 0 }}
+                      animate={{ height: "auto", opacity: 1 }}
+                      exit={{ height: 0, opacity: 0 }}
+                      transition={{ duration: 0.3, ease: "easeInOut" }}
+                      className={`pl-8 pb-8 !font-medium ${
+                        isSelected ? "bg-slate-200" : ""
+                      }`}
+                    >
+                      <div className="flex flex-col gap-1">
+                        <h3 className="font-semibold pt-4">Google Platz 1</h3>
+                        <a href="#" className="block text-neutral-400">
+                          SEO Berater für Google Rankings
+                        </a>
+                        <a href="#" className="block text-neutral-400">
+                          Google SEO Optimierung
+                        </a>
+                        <div
+                          className="text-neutral-400"
+                          onClick={() => setIsMenuOpen((prev) => !prev)}
+                        >
+                          <LinkSeoCheck />
+                        </div>
                       </div>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <h3 className="mt-4 font-semibold">ChatGPT Platz 1</h3>
-                      <a href="#" className="mb-1 block text-neutral-400">
-                        GEO-Berater für KI Sichtbarkeit
-                      </a>
-                      <a href="#" className="mb-1 block text-neutral-400">
-                        ChatGPT SEO
-                      </a>
-                      <a href="#" className="block text-neutral-400">
-                        KI Sichtbarkeit
-                      </a>
-                      <div
-                        className="text-neutral-400"
-                        onClick={() => setIsMenuOpen((prev) => !prev)}
-                      >
-                        <LinkGeoCheck />
+                      <div className="flex flex-col gap-1">
+                        <h3 className="mt-4 font-semibold">ChatGPT Platz 1</h3>
+                        <a href="#" className="block text-neutral-400">
+                          GEO-Berater für KI Sichtbarkeit
+                        </a>
+                        <a href="#" className="block text-neutral-400">
+                          ChatGPT SEO
+                        </a>
+                        <a href="#" className="block text-neutral-400">
+                          KI Sichtbarkeit
+                        </a>
+                        <div
+                          className="text-neutral-400"
+                          onClick={() => setIsMenuOpen((prev) => !prev)}
+                        >
+                          <LinkGeoCheck />
+                        </div>
                       </div>
-                    </div>
-                    <div className="flex flex-col gap-2">
-                      <h3 className="mt-4 font-semibold">Google Werbung</h3>
-                      <a href="#" className="block text-neutral-400">
-                        Google Ads Berater für Google-Klicks
-                      </a>
-                      <a href="#" className="block text-neutral-400">
-                        Kostenloser Google-Ads-Kurs
-                      </a>
-                    </div>
-                  </motion.ol>
-                )}
-              </AnimatePresence>
+                      <div className="flex flex-col gap-1">
+                        <h3 className="mt-4 font-semibold">Google Werbung</h3>
+                        <a href="#" className="block text-neutral-400">
+                          Google Ads Berater für Google-Klicks
+                        </a>
+                        <a href="#" className="block text-neutral-400">
+                          Kostenloser Google-Ads-Kurs
+                        </a>
+                      </div>
+                    </motion.ol>
+                  )}
+                </AnimatePresence>
 
-              <Link
-                to={"/Preise"}
-                onClick={() => setIsMenuOpen((prev) => !prev)}
-              >
-                Preispakete
-              </Link>
-              <Link
-                to={"/Referenzen"}
-                onClick={() => setIsMenuOpen((prev) => !prev)}
-              >
-                Referenzen
-              </Link>
-              <Link
-                to={"/Videos"}
-                onClick={() => setIsMenuOpen((prev) => !prev)}
-              >
-                Videos
-              </Link>
-              <Link to={"/KI-Tracker"}>KI-Tracker</Link>
+                <Link
+                  to={"/Preise"}
+                  onClick={() => setIsMenuOpen((prev) => !prev)}
+                  className="pt-6 pb-2 mx-8 border-b-2 border-slate-200"
+                >
+                  Preispakete
+                </Link>
+                <Link
+                  to={"/Referenzen"}
+                  onClick={() => setIsMenuOpen((prev) => !prev)}
+                  className="pt-4 pb-2 mx-8 border-b-2 border-slate-200"
+                >
+                  Referenzen
+                </Link>
+                <Link
+                  to={"/Videos"}
+                  onClick={() => setIsMenuOpen((prev) => !prev)}
+                  className="pt-4 pb-2 mx-8 border-b-2 border-slate-200"
+                >
+                  Videos
+                </Link>
+                <Link
+                  to={"/KI-Tracker"}
+                  className="pt-4 pb-2 mx-8 border-b-2 border-slate-200"
+                >
+                  KI-Tracker
+                </Link>
+              </div>
             </ol>
-            <div className="pt-8 flex items-center gap-4 text-lg">
+            <div className="pt-8 pl-8 flex items-center gap-4 text-lg">
               <Phone color="black" className="size-5" />
               <span>+49 176 55 10 93 83</span>
             </div>
-            <div className="pt-4 flex items-center gap-4 text-lg">
+            <div className="mt-4 pl-8 flex items-center gap-4 text-lg">
               <Mail color="" className="size-5" />
               <span>jan@jankroesche.de</span>
             </div>
