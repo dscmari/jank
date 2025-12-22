3d8b41df92550fc437fb9229c260940131c51e10
Marian Noekel <mariannoekel@gmail.com>
2025-12-22 20:34:27 +0100

    change website check form design

--- src/components/WebsiteCheck.tsx
+++ src/components/WebsiteCheck.tsx
@@ -35,7 +35,7 @@ export default function WebsiteCheck({ isGeo, className }: Props) {
           </div>
         </div>
         <div className="lg:flex lg:flex-col 2xl:justify-center lg:py-8 lg:max-w-1/2">
-          <h2 className="!mb-8 lg:!mb-4 !text-2xl lg:!text-4xl/10 lg:text-custom-red">
+          <h2 className="!mb-8 lg:!mb-4 !text-2xl lg:!text-4xl/10 tracking-tight lg:text-custom-red">
             {isGeo ? "Kostenloser KI GEO Check" : "Kostenloser Website Check"}
           </h2>
           {isGeo ? (
